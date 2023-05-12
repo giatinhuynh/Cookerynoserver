55f0a02736329a037bfd0cbc0f9a4729b03897b2
Restyled.io <commits@restyled.io>
2023-05-12 13:06:52 +0000

    Restyled by prettier

--- index.js
+++ index.js
@@ -5,47 +5,41 @@ const customPrompt = document.querySelector(".custom-prompt");
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
 
-  const expertise = document.querySelector('#expertise').value;
-  const complexity = document.querySelector('#complexity').value;
-  const ingredients = document.querySelector('#ingredients').value;
-  const budget = document.querySelector('#budget').value;
-  const time = document.querySelector('#time').value;
-  const scale = document.querySelector('#scale').value;
-  const nutrition = document.querySelector('#nutrition').value;
-  const taste = document.querySelector('#taste').value;
-  const dietary_restrictions =
-      document.querySelector('#dietary-restrictions').value;
+  const expertise = document.querySelector("#expertise").value;
+  const complexity = document.querySelector("#complexity").value;
+  const ingredients = document.querySelector("#ingredients").value;
+  const budget = document.querySelector("#budget").value;
+  const time = document.querySelector("#time").value;
+  const scale = document.querySelector("#scale").value;
+  const nutrition = document.querySelector("#nutrition").value;
+  const taste = document.querySelector("#taste").value;
+  const dietary_restrictions = document.querySelector(
+    "#dietary-restrictions"
+  ).value;
 
-  const prompt =
-      `You are a cooking chatbot that can help me prepare the perfect meal! Given the expertise level of ${
-          expertise}, complexity of ${complexity}, budget of ${
-          budget}, preparation time of ${time} minutes, serving scale of ${
-          scale}, nutrition preference of ${nutrition}, taste preference of ${
-          taste}, dietary restrictions of ${
-          dietary_restrictions}, and available ingredients of ${
-          ingredients}, please provide a suitable grocery list, recipe, and cooking instructions for a meal.`;
+  const prompt = `You are a cooking chatbot that can help me prepare the perfect meal! Given the expertise level of ${expertise}, complexity of ${complexity}, budget of ${budget}, preparation time of ${time} minutes, serving scale of ${scale}, nutrition preference of ${nutrition}, taste preference of ${taste}, dietary restrictions of ${dietary_restrictions}, and available ingredients of ${ingredients}, please provide a suitable grocery list, recipe, and cooking instructions for a meal.`;
   const submitButton = document.querySelector(".submit-button");
   submitButton.textContent = "Please wait...";
   submitButton.disabled = true;
 
   try {
-    const response = await fetch('https://api.openai.com/v1/chat/completions', {
-      method : 'POST',
-      headers : {
-        'Content-Type' : 'application/json',
-        'Authorization' :
-            'Bearer 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' // Replace
-                                                                         // with
-                                                                         // your
-                                                                         // OpenAI
-                                                                         // API
-                                                                         // key
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "Bearer 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", // Replace
+        // with
+        // your
+        // OpenAI
+        // API
+        // key
       },
-      body : JSON.stringify({
-        "model" : "gpt-3.5-turbo",
-        "messages" : [ {"role" : "user", "content" : prompt} ],
-        "temperature" : 1
-      })
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: prompt }],
+        temperature: 1,
+      }),
     });
 
     if (!response.ok) {
@@ -70,27 +64,26 @@ sendPromptButton.addEventListener("click", sendCustomPrompt);
 async function sendCustomPrompt() {
   const customPromptInput = document.querySelector("#customPromptInput");
   const customPrompt = customPromptInput.value.trim();
-  if (!customPrompt)
-    return;
+  if (!customPrompt) return;
 
   try {
-    const response = await fetch('https://api.openai.com/v1/chat/completions', {
-      method : 'POST',
-      headers : {
-        'Content-Type' : 'application/json',
-        'Authorization' :
-            'Bearer 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' // Replace
-                                                                         // with
-                                                                         // your
-                                                                         // OpenAI
-                                                                         // API
-                                                                         // key
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "Bearer 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", // Replace
+        // with
+        // your
+        // OpenAI
+        // API
+        // key
       },
-      body : JSON.stringify({
-        "model" : "gpt-3.5-turbo",
-        "messages" : [ {"role" : "user", "content" : customPrompt} ],
-        "temperature" : 1
-      })
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: customPrompt }],
+        temperature: 1,
+      }),
     });
 
     if (!response.ok) {
@@ -121,8 +114,7 @@ function displayRecipe(data) {
   recipeElement.innerHTML = "";
 
   const message = data.choices[0].message.content.trim().replace(/\n/g, "<br>");
-  recipeElement.innerHTML =
-      `<div class="message"><div class="bot-message">${message}</div></div>`;
+  recipeElement.innerHTML = `<div class="message"><div class="bot-message">${message}</div></div>`;
   customPrompt.style.display = "block";
   form.reset();
 }
